bfc70c83674db9229cc2409a5708a91fb110177d
NeverThyme <geovier@gmail.com>
2018-11-12 12:50:46 -0500

    updated board class

--- board.java
+++ board.java
@@ -18,11 +18,17 @@ public class Board {
   //Create a gameboard, and fill it with white squares.
   Square gameboard[][] = new Square;
   Square token = new Square;
-  for (int i = 0; i < gameboard.size(); ++i) {
-    for (int j = 0; j < gameboard.size(); ++j) {
+  for(int i = 0; i < 20; ++i) {
+    for(int j = 0; j < 20; ++j) {
       gameboard[i][j] = token;
     }
   }
 
-  
+  int finish = 0;
+  while (finish == 0) {
+    player1Turn();
+    comp1Turn();
+    player2Turn();
+    comp2Turn();
+  }
 }
