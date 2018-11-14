b04ed9b1ad7e6b198111dd16a627ca74db92a22b
NeverThyme <geovier@gmail.com>
2018-11-14 13:48:00 -0500

    "Fixed bugs."

--- board.java
+++ board.java
@@ -1,34 +1,36 @@
 import java.util.*;
 import java.io.*;
-import java.lang.Object;
+import java.awt.Color;
 
 class Square {
-  boolean taken = FALSE;
+  boolean taken = false;
   int value = 0;
   int xLoc;
   int yLoc;
-  Color color = white;
+  Color color;
 }
 
 public class Board {
+  void makeBoard(){
   //Create a gameboard, and fill it with white squares.
-  Square[][] gameboard = new Square[20][20];
-  Square placehold = new Square;
-  for(int i = 0; i < 20 ; ++i) {
+    Square[][] gameboard = new Square[20][20];
+    Square placehold = new Square();
+
+    for(int i = 0; i < 20 ; ++i) {
     for(int j = 0; j < 20; ++j) {
       gameboard[i][j] = placehold;
+      }
     }
-  }
 
-  int finish = 1;
-  printf("created board");
-  while (finish == 0) {
-    player1Turn();
-    comp1Turn();
-    player2Turn();
-    comp2Turn();
-    if(endGame()) {
-      finish = 1;
+    int finish = 1;
+    while (finish == 0) {
+      player1Turn();
+      comp1Turn();
+      player2Turn();
+      comp2Turn();
+      if(endGame()) {
+        finish = 1;
+      }
     }
   }
 }
