1d08af19a573a7aac2f40b4e8d9b7c20f7391e7d
Ajwise <43044385+Ajwise@users.noreply.github.com>
2018-11-17 01:27:43 -0800

    Update Player.java
    Lots of minor changes, fixed inheritance issues. Only one turn method is necessary now. Currently the only piece is a 1 by 1 square but should work for any pieces added to the piece database

--- Player.java
+++ Player.java
@@ -1,44 +1,56 @@
 import java.lang.Object;
 import java.util.Scanner;
+import java.awt.Color;
 
-public class Player extends Board {
+public class Player {
 
     int points;
     Color pieceColor;
-    Square[][] pieces;
+    Square[][] pieces = new Square [1][1];
 
 //======================================================================================================================
 
     public Player(Color color){
         pieceColor = color;
+        
+        //default single square added until all pieces are added.
+        
+        pieces[0][0] = new Square();
     }
 
+    //temp default constructor to fix computer heirarchy problems
+    public Player(){
+       
+    }
 //======================================================================================================================
 
-    public void player1Turn() {
+    public Board playerTurn(Board board) {
 
         //prompt player to pick a piece
         System.out.println("Pick a piece to play.");
 
         //player chooses piece
         //until GUI this will be a random piece
-        Square[] player1Pick = pieces[1];
+        Square[] player1Pick = pieces[0];
 
         //player chooses location for piece
         int player1XPick = chooseXLocation();
         int player1YPick = chooseYLocation();
 
+     
 
         //validate location
-        boolean canPlacePiece = checkPlacement(player1XPick, player1YPick, player1Pick);
+        boolean canPlacePiece = checkPlacement(player1XPick, player1YPick, player1Pick, board);
 
-
+        
         if (canPlacePiece){
-            placePiece(player1XPick, player1YPick, player1Pick, pieceColor);
+        	
+            board = placePiece(player1XPick, player1YPick, player1Pick, pieceColor, board);
+            
         }else{
-            System.out.println("The piece does not fit there.")
+            System.out.println("The piece does not fit there.");
         }
-
+        
 
         while (!canPlacePiece){
 
@@ -46,19 +58,22 @@ public class Player extends Board {
             player1YPick = chooseYLocation();
 
             //validate location
-            canPlacePiece = checkPlacement(player1XPick, player1YPick, player1Pick);
+            canPlacePiece = checkPlacement(player1XPick, player1YPick, player1Pick, board);
         }
         //update pieces array
+        //currently does nothign because we only have one piece which is a single square
         removePiece(player1Pick);
-
+       
 
         System.out.println("Player 1 went.");
+        return board;
 
 
     }
 
 //======================================================================================================================
 
+    /*
     public void player2Turn() {
 
         //prompt player to pick a piece
@@ -98,6 +113,7 @@ public class Player extends Board {
 
 
     }
+    */
 
 //======================================================================================================================
 
@@ -108,14 +124,14 @@ public class Player extends Board {
 
 
 
-
+//////////////switched to 0-19 because our database is zero indexed
 
     int chooseXLocation(){
-        System.out.println("Pick a column for your piece (1-20): ");
+        System.out.println("Pick a column for your piece (0-19): ");
         Scanner input = new Scanner(System.in);
         int player1XPick = input.nextInt();
-        while (player1XPick < 1 || player1XPick > 20){
-            System.out.println("Please enter a value 1-20: ");
+        while (player1XPick < 0 || player1XPick > 19){
+            System.out.println("Please enter a value 0-19: ");
             player1XPick = input.nextInt();
         }
         return player1XPick;
@@ -123,39 +139,49 @@ public class Player extends Board {
 
 
     int chooseYLocation(){
-        System.out.println("Pick a row for your piece (1-20): ");
+        System.out.println("Pick a row for your piece (0-19): ");
         Scanner input = new Scanner(System.in);
         int player1YPick = input.nextInt();
-        while (player1YPick < 1 || player1YPick > 20){
-            System.out.println("Please enter a value 1-20: ");
+        while (player1YPick < 0 || player1YPick > 19){
+            System.out.println("Please enter a value 0-19: ");
             player1YPick = input.nextInt();
         }
         return player1YPick;
     }
 
-    boolean checkPlacement(int x, int y, Square[] piece){
-        if (!Board.getTaken(x, y)){
+    boolean checkPlacement(int x, int y, Square[] piece, Board board){
+    	
+    	
+            
+        if (!board.getTaken(x, y)){
             for (int i = 0; i < piece.length; i++) {
-                if(!Board.getTaken(x + piece[i].getX, y + piece[i].getY)){
+                if(!Board.getTaken(x + piece[i].xLoc, y + piece[i].yLoc)){
 
                 }else{
                     return false;
                 }
             }
-        }else{
+        }
+        else{
             return false;
         }
+       
         return true;
+        
     }
 
     //also adds points
-    void placePiece(int x, int y, Square[] piece, Color color) {
+    Board placePiece(int x, int y, Square[] piece, Color color, Board board) {
         for (Square s : piece){
-            board[s[i.getX]][s[i].getY].capture;
-            board[s[i.getX]][s[i].getY].setColor(color);
+        	 
+        	board.setSquare(s.xLoc, s.yLoc, s.color);
+        	
             points += 1;
-
+            
+            
+			
         }
+        return board;
     }
 
 
@@ -176,12 +202,17 @@ public class Player extends Board {
 
     void removePiece(Square[] piece){
         for(int i = 0; i < pieces.length; i++){
+        	
+        	/* commented out until comparePieces implemented
             if(comparePieces(pieces[i], piece)){
                 pieces[i] = null;
+               
             }
+            */
         }
+            
+        
     }
-
 
 
 
