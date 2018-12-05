dbb186c47152b0456eccdc8bb0136cdf92445b72
Kyle Austin Michel <kyle.michel@uvm.edu>
2018-12-05 10:27:30 -0500

    Square.java modified to fix player piece array

--- Square.java
+++ Square.java
@@ -3,10 +3,19 @@ import java.awt.Color;
 
 public class Square {
 
-
+  
   boolean taken = false;
   int value = 0;
   int xLoc;
   int yLoc;
   Color color = new Color(0,0,0);
+  
+  public Square() {
+	  
+  }
+  public Square(int x, int y) {
+	  xLoc = x;
+	  yLoc = y;
+}
+
 }
